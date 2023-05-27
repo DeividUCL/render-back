de0cb2e8bec72a963840970ccc8a0c5c944ba76b
IsraelSanatana <isferna007@gmail.com>
2023-05-27 19:23:28 -0300

    alteração

--- src/controller/alimentos.controller.js
+++ src/controller/alimentos.controller.js
@@ -4,7 +4,7 @@ export default class AlimentosController {
 
     async get(req, res) {
         const result = await knex.select("*").table("alimentos")
-        const response = result.map((item) => { return { id: item.id, nome: item.alimento } })
+        const response = result.map((item) => { return { id: item.id, nome: item.alimento, temperatura: item.temperatura, tempo: item.tempo } })
         return res.json({ data: response }).status(200);
     }
 
